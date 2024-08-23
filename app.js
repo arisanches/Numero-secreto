e657def71904e3a37bf697223c6661700c79d003
ariel ! 3 <106552438+arisanches@users.noreply.github.com>
2024-08-23 15:41:19 -0300

    4

---
+++ app.js
@@ -0,0 +1,37 @@
+alert('Boas vindas ao numero secreto');
+let numeroSecreto = 6;
+console.log(numeroSecreto)
+let chute
+
+while(chute != numeroSecreto) {
+     chute = prompt('Escolha um número entre 1 e 10');
+
+     if (chute == numeroSecreto) {
+     alert(`Isso aí! Você descobriu o número secreto
+            ${numeroSecreto}`);
+
+        } else {
+ if (chute > numeroSecreto) {
+
+
+ 
+      alert (`O numeroSecreto é menor que ${chute}`);
+
+
+ 
+      } else {
+
+
+ 
+      alert (`O numeroSecreto é maior que ${chute}`);
+
+
+ 
+      }
+
+
+ 
+  }
+
+
+}
